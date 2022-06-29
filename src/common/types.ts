7ff19793545ea3e40e2e074a3c5202af1528dbb7
Dave Lilly <dave@lillys.org>
2022-06-29 11:57:45 -0500

    add hubId to context for handlebars mapping

--- src/common/types.ts
+++ src/common/types.ts
@@ -78,6 +78,7 @@ export const getMapping = async (context: ImportContext): Promise<Mapping> => {
         url: context.environment.url,
         cms: {
             hub: context.hub.name!,
+            hubId: context.hub.id!,
             stagingApi: context.hub.settings?.virtualStagingEnvironment?.hostname || '',
             imageHub: context.config?.cms.imageHub,
             repositories: _.zipObject(_.map(repositories, r => r.name!), _.map(repositories, 'id')),
